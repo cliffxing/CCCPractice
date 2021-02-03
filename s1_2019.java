361d1c770581574f99221a718c13f0e4683168b3
cliffxing <76663573+cliffxing@users.noreply.github.com>
2021-02-03 13:54:21 -0500

    s1 and s2 of 2019, and s1 of 2020

---
+++ s1_2019.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+/**
+ * Write a description of class s1_2019 here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class s1_2019
+{
+    // instance variables - replace the example below with your own
+
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        int r;
+        int l;
+
+        int [][] grid = new int[2] [2];
+        grid[0][0]=1;
+        grid[0][1]=2;
+        grid[1][0]=3;
+        grid[1][1]=4;
+
+        for (int i=0; i<s.length();i++)
+        {
+            char c = s.charAt(i);
+            if (c == 'H')
+            {
+                r = grid[0][0];
+                l = grid[0][1];
+
+                grid[0][0] = grid[1][0];
+                grid[0][1] = grid [1][1];
+                
+                grid[1][0] = r;
+                grid [1][1] = l;
+
+            }
+
+            else if (c=='V')
+            {
+                r = grid[0][0];
+                l = grid[1][0];
+
+                grid[0][0] = grid[0][1];
+                grid[1][0] = grid [1][1];
+                
+                grid[0][1] = r;
+                grid [1][1] = l;
+            }
+        }
+
+
+        for (int y=0; y<2; y++)
+        {
+            for (int x =0; x<2; x++)
+            {
+                System.out.print(grid[y][x]);
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+        
+
+    }
+}
---
+++ s1_2020.java
@@ -0,0 +1,105 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.lang.Math;
+import java.util.Comparator;
+
+public class s1_2020 {
+
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        double speed;
+        s.nextLine();
+        double x;
+        double y;
+        double x2;
+        double y2;
+
+        double[] [] array = new double[n][2];
+
+        for (int i=0; i<n; i++)
+        {
+            String[] line = s.nextLine().split(" ");
+            array[i][0] = Double.valueOf(line[0]);
+            array[i][1] = Double.valueOf(line[1]);
+        }
+
+        Arrays.sort(array, new Comparator<double[]>()
+        {
+            public int compare (double[] a, double[] b)
+            {
+                return Double.compare(a[0], b[0]);
+            }
+        }
+        );
+        
+        
+      boolean hasStarted = false;
+        double biggest=0;
+
+    
+        for (int i=0; i<n; i++)
+        {
+
+            if (i!= n-1)
+            {
+                
+                x = array[i][0];
+                y = array[i][1];
+
+                
+                x2 = array[i+1][0];
+                y2 = array[i+1][1];
+
+                speed = Math.abs(y2-y)/Math.abs(x2-x);
+                
+                if (hasStarted == false)
+                {
+                    biggest = speed;
+                    hasStarted = true;
+                }
+
+                else
+                {
+                    if (speed>biggest)
+                    {
+                        biggest = speed;
+                    }
+                }
+            }
+        }
+
+        System.out.println(biggest);
+
+       
+    }
+    
+}
+
+/*
+ * 
+ *  for (int i = 0; i < xarray.length; i++) {
+
+            for (int k = 1; k < xarray.length - i; k++) {
+
+                if (xarray[k - 1] > xarray[k]) {
+
+                    double temp = xarray[k - 1];
+
+                    xarray[k - 1] = xarray[k];
+
+                    xarray[k] = temp;
+
+                    temp = yarray[k-1];
+                    yarray[k - 1] = yarray[k];
+                    yarray[k] = temp;
+
+                }
+
+            }
+
+        }
+
+        
+ */
